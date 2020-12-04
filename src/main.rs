0c43271e7d7e074b33fbc2893cc770d979f5b780
Jamie Nicol <jamie@jamienicol.me>
2020-12-04 10:40:39 +0000

    Tidy up day 1

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,42 @@ use std::io::{BufRead, BufReader};
 
 #[derive(Error, Debug)]
 enum AocError {
-    #[error("No n entries exist with a sum of 2020")]
+    #[error("No entries exist with a matching sum")]
     BadEntries,
 }
 
-fn day_1() -> Result<i32> {
-    let file = File::open("res/day_1_input")?;
-    let reader = BufReader::new(file);
-    let entries: Result<Vec<_>, _> = reader.lines().map(|l| l.unwrap().parse::<i32>()).collect();
-    let entries = entries?;
-    println!("{:?}", entries);
-
-    for combination in entries.into_iter().combinations(3) {
-        println!("{:?}", combination);
-        let sum: i32 = combination.iter().sum();
-        if sum == 2020 {
-            return Ok(combination.iter().product());
+fn find_product_of_entries_with_sum(entries: &[i32], num_entries: usize, sum: i32) -> Result<i32> {
+    for combination in entries.iter().combinations(num_entries) {
+        if combination.iter().cloned().sum::<i32>() == sum {
+            return Ok(combination.iter().cloned().product());
         }
     }
 
     Err(AocError::BadEntries.into())
 }
 
+fn day_1() -> Result<()> {
+    let file = File::open("res/day_1_input")?;
+    let reader = BufReader::new(file);
+    let entries = reader
+        .lines()
+        .map(|l| l.unwrap().parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()?;
+
+    println!(
+        "Day 1, part 1: {}",
+        find_product_of_entries_with_sum(&entries, 2, 2020)?
+    );
+    println!(
+        "Day 1, part 2: {}",
+        find_product_of_entries_with_sum(&entries, 3, 2020)?
+    );
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    println!("Day 1: {}", day_1()?);
+    day_1()?;
 
     Ok(())
 }
