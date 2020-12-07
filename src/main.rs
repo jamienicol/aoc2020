93f940b923761015df46fd9a1ac72da00b7a4365
Jamie Nicol <jamie@jamienicol.me>
2020-12-07 12:56:20 +0000

    Day 4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use anyhow::{anyhow, Result};
 use itertools::Itertools;
 use nom::{
-    character::complete::{alpha1, anychar, char, digit1, space1},
+    branch::alt,
+    bytes::complete::{tag, take_till},
+    character::complete::{alpha1, anychar, char, digit1, multispace1, space1},
     combinator::map_res,
+    multi::separated_list1,
+    sequence::separated_pair,
     IResult,
 };
 
+use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
 fn find_product_of_entries_with_sum(entries: &[i32], num_entries: usize, sum: i32) -> Result<i32> {
@@ -154,18 +159,16 @@ fn day_3() -> Result<()> {
             char => Err(anyhow!("Unexpected input {:?} in tree map", char)),
         })
         .collect::<Result<Vec<bool>>>()?;
-    assert_eq!(map_width * map_height, map.len(), "Unexpected size of tree map");
+    assert_eq!(
+        map_width * map_height,
+        map.len(),
+        "Unexpected size of tree map"
+    );
 
     let tree_count = num_trees_encountered(&map, (map_width, map_height), (3, 1));
     println!("Day 3, part 1: {}", tree_count);
 
-    let slopes = [
-        (1, 1),
-        (3, 1),
-        (5, 1),
-        (7, 1),
-        (1, 2),
-    ];
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
     let product: usize = slopes
         .iter()
         .map(|slope| num_trees_encountered(&map, (map_width, map_height), *slope))
@@ -176,10 +179,56 @@ fn day_3() -> Result<()> {
     Ok(())
 }
 
+fn parse_passport_1(input: &str) -> IResult<&str, HashSet<&str>> {
+    let (input, fields) = separated_list1(
+        multispace1,
+        alt((
+            separated_pair(tag("byr"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("iyr"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("eyr"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("hgt"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("hcl"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("ecl"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("pid"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+            separated_pair(tag("cid"), tag(":"), take_till(|c| c == ' ' || c == '\n')),
+        )),
+    )(input)?;
+
+    let mut passport = HashSet::default();
+    for (field, _value) in fields {
+        passport.insert(field);
+    }
+
+    Ok((input, passport))
+}
+
+fn day_4() -> Result<()> {
+    let input = std::fs::read_to_string("res/day_4_input")?;
+
+    const REQUIRED_FIELDS: [&'static str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+    let num_valid = input
+        .split("\n\n")
+        .map(|input| {
+            parse_passport_1(input).map_err(|err| anyhow!("Error parsing passport: {:?}", err))
+        })
+        .flatten()
+        .map(|(_input, passport)| {
+            REQUIRED_FIELDS.iter().all(|field| passport.contains(field)) as usize
+        })
+        .sum::<usize>();
+
+    // 196
+    println!("Day 4, part 1: {}", num_valid);
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    // day_1()?;
-    // day_2()?;
+    day_1()?;
+    day_2()?;
     day_3()?;
+    day_4()?;
 
     Ok(())
 }
